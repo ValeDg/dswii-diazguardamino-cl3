4dd9c70cda0bc4c2ec637d51b76e18d88db19694
ValeDg <diazguardamino07@gmail.com>
2023-11-25 20:39:07 -0500

    Creacion proyecto

---
+++ src/main/java/pe/edu/cibertec/dswiicl3diazguardamino/DswiiCl3DiazguardaminoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dswiicl3diazguardamino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3DiazguardaminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3DiazguardaminoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/dswiicl3diazguardamino/DswiiCl3DiazguardaminoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dswiicl3diazguardamino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3DiazguardaminoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
